1693bc50d0abd9c254e7faebf77faf7924843825
arberx <axhindol@umich.edu>
2019-01-15 18:47:40 -0500

    added example transaction

--- scripts/testPlaid.js
+++ scripts/testPlaid.js
@@ -2,10 +2,9 @@ require('dotenv').config()
 
 const path = require('path')
 const { writeFile } = require('fs-extra')
-const { fetchTransactions } = require('../lib/fetch')
+const { fetchCategories } = require('../lib/fetch')
 
 ;(async () => {
-  const res = await fetchTransactions()
-  console.log('Transactions fetch successful!')
+  const res = await fetchCategories()
   console.log(res)
 })()
